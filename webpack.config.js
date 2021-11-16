acefbdf8690b3dbcff276a9e0fdc7107430c5c3c
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2021-11-16 11:28:27 +0530

    added template html and did cache busting using hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[hash].js",
         path: path.join(__dirname,"dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+      template : "./src/template.html"
+    })],
     module: {
         rules: [
           {
